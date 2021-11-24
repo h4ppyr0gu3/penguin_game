0a6bc8eb798c20bbd06efb2eafa3acdeee0e85f8
David Rogers <rogersdpdr@gmail.com>
2021-11-24 14:59:59 +0100

    updating git ignore

---
+++ src/obj/arguments.d
@@ -0,0 +1,5 @@
+src/obj/arguments.o: src/arguments.c src/arguments.h src/game_params.h \
+ src/help.h
+src/arguments.h:
+src/game_params.h:
+src/help.h:
---
+++ src/obj/avatar.d
@@ -0,0 +1,2 @@
+src/obj/avatar.o: src/avatar.c src/game_params.h
+src/game_params.h:
