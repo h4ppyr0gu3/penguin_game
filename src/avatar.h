122ad0cf10c7f419aee6ab9cabdf95c2f41270cf
rbinshul <r.binshulayyil@stud.elka.pw.edu.pl>
2021-12-15 15:36:02 +0100

    Update avatar.h

--- src/avatar.h
+++ src/avatar.h
@@ -1,6 +1,12 @@
 #ifndef AVATAR_H
 #define AVATAR_H
 
+/**
+* runs a loob to ask player for an avatar and set it in the game state 
+*
+* @param game_params game state to be set by a function 
+*/
+
 #include "game_params.h"
 
 void choose_avatar(GameParams *game_params); // updates player array in game params struct
