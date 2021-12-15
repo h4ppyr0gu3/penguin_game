fe3e8f28d9ecc7c2dde00f1254487ffcb1084c60
rbinshul <r.binshulayyil@stud.elka.pw.edu.pl>
2021-12-15 15:44:32 +0100

    Update map_gen.h

--- src/map_gen.h
+++ src/map_gen.h
@@ -2,6 +2,11 @@
 #ifndef MAP_GEN_H
 #define MAP_GEN_H
 
+/**
+*
+*
+*/ 
+
 void generate_map(GameParams *game_params); // takes game params and updates board array
 int check_board(GameParams *game_params); // checks the array to see if there is any unassigned position 
 
