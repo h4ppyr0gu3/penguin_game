1979be953a417be31499e9639064311a3a91f958
rbinshul <r.binshulayyil@stud.elka.pw.edu.pl>
2021-12-15 16:25:22 +0100

    Update printer.h

--- src/printer.h
+++ src/printer.h
@@ -1,6 +1,11 @@
 #include "game_params.h"
 #ifndef PRINTER_H
 #define PRINTER_H
+ 
+/*
+* set the board and print current numbers
+* @param game_params set the board and print current scoreboard
+*/
 
 void print_stage(GameParams *game_params); // print board array to screen
 void print_scoreboard(GameParams *game_params); // print scoreboard to screen
